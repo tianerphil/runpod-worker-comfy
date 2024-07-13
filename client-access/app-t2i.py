abd6ae20036b440cfe8bd792d51c2335c328fc2f
tianerphil <zhang.philips@qq.com>
2024-07-13 11:56:03 +0400

    update MYREADME.

--- client-access/app-t2i.py
+++ client-access/app-t2i.py
@@ -7,7 +7,7 @@ from ComfyUIClient import ComfyUIClient
 
 # Set up logging
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)'
 )
 
